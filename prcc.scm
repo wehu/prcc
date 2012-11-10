1cb150754d000170c6e603b6615530644098ab4e
Wei Hu <huwei04@hotmail.com>
2012-11-10 16:18:25 +0800

    refine  Changes to be committed: 	modified:   prcc.scm

--- prcc.scm
+++ prcc.scm
@@ -386,7 +386,7 @@
        (lambda (ctxt)
          ((lambda (c)
             (check-procedure 'lazy p)
-            (p c)) ctxt)))))
+            (apply-c p c)) ctxt)))))
 
   ;; regexp
   (define (regexp-parser r)
